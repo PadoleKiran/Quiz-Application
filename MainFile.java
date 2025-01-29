bdfd3185b8e3d15740f78ffb17efaf24fcbb9aa7
kiran <shildking999@gmail.com>
2025-01-29 11:47:44 +0530

    This is version 2 of quiz application

---
+++ MainFile.java
@@ -0,0 +1,6 @@
+public class MainFile extends QuestionPrinter {
+    public static void main(String[] args) {
+        QuestionPrinter.questionlistprint();
+    }
+}
+
---
+++ QuestionsList.java
@@ -0,0 +1,25 @@
+public class QuestionsList {
+    
+    public static String[][] questions = {
+        {"1", "What is the capital of France?", "Paris", "London", "Berlin", "Madrid", "Paris"},
+        {"2", "What is 2 + 2?", "3", "4", "5", "6", "4"},
+        {"3", "Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", "Mars"}
+    };
+
+    
+    // public static void addnewquestion() {
+    //     String[][] newQuestionsArray = new String[questions.length + 1][];
+        
+    //     for(int i=0; i<questions.length; i++){
+    //         newQuestionsArray[i] = questions[i];
+    //     }
+    // }
+
+    public static String[][] getQuestions() {
+        return questions;
+    }
+
+
+}
+ 
+
